a41f93c5518f5c7c141f134bdb6058df695bb9e5
Lucas Pascoal <olucaspascoal@gmail.com>
2022-02-22 00:24:56 -0300

    Análise de funcionalidade

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -25,5 +25,19 @@ export class MainComponent implements OnInit {
   remover(posic: any){
     this.listaAtividade.splice(posic, 1)
   }
-  
+  del(i:any){
+    console.log("DEL --> " + i);
+    const tash = document.createElement("del");
+    const node = document.createTextNode("This is new.");
+    const docdoc = document.getElementById(i)
+    var texto = docdoc?.innerHTML;
+    console.log(texto)
+    document.getElementById(i)?.appendChild(tash)
+    console.log(docdoc);
+    // tash.appendChild(docdoc);
+    const element = document.getElementById('item-'+i);
+    console.log(element);
+    // element.appendChild(tash);
+
+  }
 }
